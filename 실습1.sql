bdc562edd7e394f54eb1349a32f69125023d1f78
andan1 <139194353+andan1@users.noreply.github.com>
2023-09-26 11:18:30 +0900

    Add files via upload

---
+++ 실습1.sql
@@ -0,0 +1,8 @@
+SHOW DATABASES;
+USE employees;
+SHOW TABLE STATUS;
+DESC employees;
+SELECT first_name, gender FROM employees;
+
+-- 데이터베이스 이름, 테이블 이름, 필드 이름이 정확히 기억나지 않을 때 
+-- employees 테이블의 first_name 및 gender열을 찾기
